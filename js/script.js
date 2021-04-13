bf9538b5823612b4092245bc0cdc2422183aa167
Gabriel Pere Lutz <gabriel.p.lutz@gmail.com>
2021-04-13 15:32:37 -0300

    Painel de participantes ativos

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ function logar(){
     let logar = document.querySelector(".login")
     logar.classList.add("invisivel")
 }
+
+function painel(){
+    let voltar = document.querySelector(".participantesAtivos")
+    voltar.classList.toggle("invisivel")
+}

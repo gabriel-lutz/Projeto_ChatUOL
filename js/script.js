dcac58d66312132e33291b9fbc23095f3e3049ad
Gabriel Pere Lutz <gabriel.p.lutz@gmail.com>
2021-04-14 20:33:00 -0300

    Loga no servidor e mantém a conexão

--- js/script.js
+++ js/script.js
@@ -1,9 +1,49 @@
-
+let nomeUsuario;
 
 function logar(){
+    
+    nomeUsuario = { name: document.querySelector(".nome-usuario").value }
+    const solicitarLogin = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", nomeUsuario )
+    solicitarLogin.then(sucessoLogin)
+    solicitarLogin.catch(falhaLogin)
+    
+}
+
+function sucessoLogin(){
+    alert("sucesso no login")
     let logar = document.querySelector(".login")
     logar.classList.add("invisivel")
+    setInterval(solicitarMensagensServidor, 3000)
+    setInterval(manterLogado, 5000)
 }
+
+function falhaLogin(){
+    alert("O nome já está em uso, escolha outro nome!")
+}
+
+function manterLogado(){
+    axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status", nomeUsuario)
+}
+
+
+
+function solicitarMensagensServidor(){
+    const atualizar = axios.get("servidor")
+    atualizar.then(atualizarMensagens)
+    atualizar.catch(falhaAtualizarMensagens)
+}
+
+function sucessoAtualizarMensagens(mensagens){
+
+}
+
+function falhaAtualizarMensagens(){
+
+    let logar = document.querySelector(".login")
+    logar.classList.remove("invisivel")
+}
+
+
 
 function painel(){
     let voltar = document.querySelector(".participantesAtivos")
